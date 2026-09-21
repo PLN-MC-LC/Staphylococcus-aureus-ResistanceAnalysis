aadbaf878a6cffa1a3e78f1f11d0ff863efb1930
Lucas Candinho <lucas25033@ilum.cnpem.br>
2026-09-21 17:45:35 -0300

    feat: add LLM_extraction

--- LLM-Extraction/llm_extration.py
+++ LLM-Extraction/llm_extration.py
@@ -1,17 +1,21 @@
 import os
 import argparse
+import json
 from dotenv import load_dotenv
 from openai import OpenAI
+from utils_preprocess import (importar_arq)
+from utils_llm_extraction import (perguntar, extrair_llm)
+from prompts import (SYSTEM, ASSISTANT)
 
 load_dotenv()
 
 API_KEY = os.getenv("ILUMA_API_KEY")
 BASE_URL = "https://iluma.cnpem.br:4000/v1"
-MODELO = "iluma"
+MODEL = "iluma"
 TEMP_EXTRACAO = 0.5
 
-cliente = OpenAI(base_url=BASE_URL, api_key=API_KEY,
-                 timeout=60.0, max_retries=1)
+client = OpenAI(base_url=BASE_URL, api_key=API_KEY,
+                timeout=60.0, max_retries=1)
 
 
 def main():
@@ -39,4 +43,22 @@ def main():
         help="Caminho do JSON de saída.",
     )
 
+    args = parser.parse_args()
+
+    df = importar_arq(args.input, args.abstract_column)
+
+    jsons_extracted = []
+
+    for abstract in df[args.abstract_column]:
+        user = abstract
+        resposta = perguntar(client, MODEL,
+                             TEMP_EXTRACAO, user,
+                             SYSTEM, ASSISTANT)
+        json_ext = extrair_llm(resposta)
+        jsons_extracted.append(json_ext)
+
+    path = os.path.join(args.output, "extracao.json")
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(jsons_extracted, file, indent=4)
+        print(f"Sua extração está disponível em: {path}!")
     return
